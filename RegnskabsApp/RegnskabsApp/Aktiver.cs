c98063a4db9b41dae73c79f65b9aecb2b1710ce2
Newjan Thevarajah <newj0001@student.eadania.dk>
2018-03-21 10:33:47 +0100

    hej

---
+++ RegnskabsApp/RegnskabsApp/Aktiver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegnskabsApp
+{
+    class Aktiver
+    {
+        
+    }
+    class AnlaegsAktiver
+    {
+        public int ImmaterielleAktiver { get; set; }
+        public int Materielle { get; set; }
+        public int Finansielle { get; set; }
+        public AnlaegsAktiver()
+        {
+            int sum;
+            sum = ImmaterielleAktiver + Materielle + Finansielle;
+        }
+    }
+}
